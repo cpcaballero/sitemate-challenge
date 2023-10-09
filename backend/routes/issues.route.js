2031964e01fdb8bcb8f57f70b0eb8cd575239748
Carlos Caballero <carloscaballero@Carloss-MacBook-Pro.local>
2023-10-10 00:49:17 +0800

    added json as store, started file read/write, get and post update

--- backend/routes/issues.route.js
+++ backend/routes/issues.route.js
@@ -1,5 +1,20 @@
-const express = require("express"); 
+const express = require('express'); 
 const router = express.Router(); 
+const FILE_NAME = 'issues.json';
+const fs = require('fs');
+
+const readDataFromFile = () => {
+  try {
+    const data = fs.readFileSync(FILE_NAME, 'utf8');
+    return JSON.parse(data);
+  } catch (error) {
+    console.log(error)
+  }
+};
+
+const saveDataToFile = (data) => {
+  fs.writeFileSync(FILE_NAME, JSON.stringify(data), 'utf8');
+}
 
 const issues = [
   {
@@ -19,21 +34,30 @@ const issues = [
   },
 ];
 
-router.get("/", (req, res) => { 
-    res.json(issues); 
+router.get('/', (req, res) => { 
+  const data = readDataFromFile();
+    res.json(data); 
 }); 
 
-router.get("/:id", (req, res) => { 
+router.get('/:id', (req, res) => { 
   res.json(issues.filter(
     (issue) => issue.id === parseInt(req.params.id),
   )); 
 }); 
 
-router.post("/", (req, res, next) => { 
-  res.json(req.body);
+router.post('/', (req, res, next) => { 
+  const data = readDataFromFile();
+  const newData = {
+    id: data[data.length-1].id + 1,
+    title: req.body.title,
+    description: req.body.description,
+  };
+  data.push(newData);
+  saveDataToFile(data);
+  return res.send(newData);
 }); 
 
-router.delete("/:id", (req, res, next) => {
+router.delete('/:id', (req, res, next) => {
   const parsedId = parseInt(req.params.id);
   const hasIssue = issues.some(
     (issue) => issue.id === parsedId,
@@ -45,7 +69,7 @@ router.delete("/:id", (req, res, next) => {
   }
 });
 
-router.put("/", (req, res, next) => { 
+router.put('/', (req, res, next) => { 
   res.json(req.body);
 }); 
 
